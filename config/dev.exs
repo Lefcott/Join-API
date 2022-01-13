228ee00c6a17c3e08f188397c005e06025b438b0
Leandro Cotti <lefcott@hotmail.com>
2022-01-13 01:45:19 -0300

    change db pool size to 1

--- config/dev.exs
+++ config/dev.exs
@@ -5,7 +5,7 @@ config :join, Join.Repo,
   url: System.get_env("DATABASE_URL"),
   ssl: false,
   show_sensitive_data_on_connection_error: true,
-  pool_size: 2
+  pool_size: 1
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
